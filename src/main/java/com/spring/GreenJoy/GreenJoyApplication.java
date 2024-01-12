cab2a4d76eab34ed9c5a93cec2018204e4598a8b
JuHyuk Lim <nyclo0705@gmail.com>
2024-01-12 11:57:14 +0900

    chore: 스프링부트 프로젝트 생성

---
+++ src/main/java/com/spring/GreenJoy/GreenJoyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.GreenJoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenJoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenJoyApplication.class, args);
+	}
+
+}
