a44e04a431d9281f781ca9125552ab47409b1771
sonshn <sonshn@naver.com>
2024-01-14 04:33:47 +0900

    fix: User, BaseTime entity 설계

---
+++ src/main/java/com/spring/GreenJoy/global/common/NanoId.java
@@ -0,0 +1,30 @@
+package com.spring.GreenJoy.global.utils;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@AllArgsConstructor(access = AccessLevel.MODULE)
+@NoArgsConstructor
+public class NanoId implements Serializable {
+
+    @Setter(AccessLevel.PRIVATE)
+    private String id;
+
+    public static NanoId makeId() {
+        return new NanoId(NanoIdUtils.randomNanoId());
+    }
+
+    public static NanoId of(String id) {
+        return new NanoId(id);
+    }
+
+    @Override
+    public String toString(){
+        return id;
+    }
+
+}
---
+++ src/main/java/com/spring/GreenJoy/global/utils/NanoIdUtils.java
@@ -0,0 +1,2 @@
+package com.spring.GreenJoy.global.utils;public class NanoIdUtils {
+}
